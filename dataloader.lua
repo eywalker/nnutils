b3c450e870fcb5727f2b61fa6c066e13ee50e23d
Edgar Y. Walker <edgar.walker@gmail.com>
2016-06-05 00:05:38 -0500

    Support sharing across threads

--- dataloader.lua
+++ dataloader.lua
@@ -1,5 +1,4 @@
 require 'torch'
-require 'nnutils.minibatch'
 require 'nnutils.processingchain'
 local paths = require 'paths'
 
@@ -9,33 +8,38 @@ nnutils = nnutils or {}
 local DataLoader = torch.class('nnutils.DataLoader')
 
 function DataLoader:__init(arg)
-  self.train_preproc = nnutils.ProcessingChain()
-  self.test_preproc = nil
-  self.nBatches = nil
-  self.batchSize = nil
+  self.trainPC = nnutils.ProcessingChain()
+  self.testPC = nnutils.ProcessingChain()
+  self._nBatches = torch.Tensor(1)
+  self._batchSize = torch.Tensor(1)
+
   -- empty trainset
   self.trainsetData = torch.Tensor()
   self.trainsetLabels = torch.Tensor()
-  self.trainsetSize = 0
+  self._trainsetSize = torch.Tensor(1)
+
   -- empty testest
   self.testsetData = torch.Tensor()
   self.testsetLabels = torch.Tensor()
-  self.testsetSize = 0
+  self._testsetSize = torch.Tensor(1)
+
+  -- epoch indicies
   self.epochIndicies = torch.LongTensor()
 end
+
 
 function DataLoader:setTrainset(trainsetData, trainsetLabels)
   assert(trainsetData:size(1) == trainsetLabels:size(1), "Sizes of data and labels don't match!")
   self.trainsetData:resize(trainsetData:size()):copy(trainsetData)
   self.trainsetLabels:resize(trainsetLabels:size()):copy(trainsetLabels)
-  self.trainsetSize = trainsetData:size(1)
+  self:setTrainsetSize(trainsetData:size(1))
 end
 
 function DataLoader:setTestset(testsetData, testsetLabels)
   assert(testsetData:size(1) == testsetLabels:size(1), "Sizes of data and labels don't match!")
   self.testsetData = testsetData
   self.testsetLabels = testsetLabels
-  self.testsetSize = testsetData:size(1)
+  self:setTestsetSize(testsetData:size(1))
 end
 
 function DataLoader:getTrainset(indicies)
@@ -62,15 +66,23 @@ function DataLoader:getTestset(indicies)
   return data, labels
 end
 
+function DataLoader:getProcessedTrainset(indicies)
+  return self.trainPC:forward(self:getTrainset(indicies))
+end
+
+function DataLoader:getProcessedTestset(indicies)
+  return self.testPC:forward(self:getTestset(indicies))
+end
+
 -- randomly sample `quantity` samples from trainset
 function DataLoader:sampleTrainset(quantity)
-  local indicies = torch.LongTensor(quantity):random(1, self.trainsetSize)
-  return self:getTrainset(indicies)
+  local indicies = torch.LongTensor(quantity):random(1, self:getTrainsetSize())
+  return self:getProcessedTrainset(indicies)
 end
 
 function DataLoader:sampleTestset(quantity)
-  local indicies = torch.LongTensor(quantity):random(1, self.testsetSize)
-  return self:getTestset(indicies)
+  local indicies = torch.LongTensor(quantity):random(1, self:getTestsetSize())
+  return self:getProcessedTestset(indicies)
 end
 
 -- Start a new epoch, resetting the batch size
@@ -78,17 +90,43 @@ end
 function DataLoader:startEpoch(batchSize, nBatches)
   if (randomize==nil) then randomize = true end
   batchSize = batchSize or 100
-  self.batchSize = batchSize
-  self.nBatches = math.ceil(self.trainsetSize / self.batchSize)
+  self._batchSize[1] = batchSize
+  self._nBatches[1] = math.ceil(self:getTrainsetSize() / self:batchSize())
   if randomize then
-    self.epochIndicies:randperm(self.trainsetSize)
+    self.epochIndicies:randperm(self:getTrainsetSize())
   end
 end
 
 function DataLoader:getMiniBatch(batchNumber)
-  local start = (batchNumber - 1) * self.batchSize + 1
-  local stop = math.min((batchNumber) * self.batchSize, self.trainSet:size())
+  local start = (batchNumber - 1) * self:batchSize() + 1
+  local stop = math.min((batchNumber) * self:batchSize(), self:getTrainsetSize())
   local batchIndicies = self.epochIndicies[{{start, stop}}]
-  local data, labels = self:getTrainset(batchIndicies)
-  return MiniBatch(data, labels)
+  return self:getProcessedTrainset(batchIndicies)
 end
+
+---------------- setters and getters --------------------------
+function DataLoader:getTrainsetSize()
+  return self._trainsetSize[1]
+end
+
+function DataLoader:setTrainsetSize(n)
+  self._trainsetSize[1] = n
+end
+
+function DataLoader:getTestsetSize()
+  return self._testsetSize[1]
+end
+
+function DataLoader:setTestsetSize(n)
+  self._testsetSize[1] = n
+end
+
+function DataLoader:batchSize()
+  return self._batchSize[1]
+end
+
+function DataLoader:nBatches()
+  return self._nBatches[1]
+end
+
+
