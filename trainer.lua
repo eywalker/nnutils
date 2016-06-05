1a379a73957da93cf792cb5c2a20d0ddaa148f45
Edgar Y. Walker <edgar.walker@gmail.com>
2016-06-05 01:57:36 -0500

    Use optim package for training

--- trainer.lua
+++ trainer.lua
@@ -1,4 +1,5 @@
 require 'torch'
+require 'optim'
 local threads = require 'threads'
 
 threads.Threads.serialization('threads.sharedserialize')
@@ -9,63 +10,93 @@ local Trainer = torch.class('nnutils.Trainer')
 
 function Trainer:__init(model, criterion, dataLoader)
   self.model = model
+  self.parameters = nil
+  self.gradParameters = nil
   self.criterion = criterion
   self.dataLoader = dataLoader
+  self.optimState = {
+    learningRate = 0.01,
+    learningRateDecay = 0.0,
+    momentum = 0.0,
+    dampening = 0.0,
+    weightDecay = 0.0
+  }
 
-  self.is_cuda = nil -- starts out not in cuda
+  -- initialize to float
   self:float()
+end
+
+function Trainer:getParameters()
+  local parameters, gradParameters = self.model:getParameters()
+  self.parameters = parameters
+  self.gradParameters = gradParameters
 end
 
 function Trainer:cuda()
   self.model:cuda()
   self.criterion:cuda()
+  self:getParameters()
   self.is_cuda = true
 end
 
 function Trainer:float()
   self.model:float()
   self.criterion:float()
+  self:getParameters()
   self.is_cuda = false
 end
 
-function Trainer:double()
-  self.model:double()
-  self.criterion:double()
-  self.is_cuda = false
-end
 
 function Trainer:train(batchSize, eta, nEpochs)
   nEpochs = nEpochs or 1
   eta = eta or 0.01
   local data, labels
   if self.is_cuda then
-    data = torch.CudaTensor()
-    labels = torch.CudaTensor()
+    self.data = torch.CudaTensor()
+    self.labels = torch.CudaTensor()
   end
   local model = self.model
   local criterion = self.criterion
   for n=1,nEpochs do
+    self.loss_epoch = 0.0
     self.dataLoader:startEpoch(batchSize)
     local nBatches = self.dataLoader:nBatches()
     for i=1,nBatches do
       local data_cpu, labels_cpu = self.dataLoader:getMiniBatch(i)
-      if self.is_cuda then
-        -- copy data to GPU
-        data:resize(data_cpu:size()):copy(data_cpu)
-        labels:resize(labels_cpu:size()):copy(labels_cpu)
-      else
-        -- working on cpu, so simply refer to batch data
-        data = data_cpu
-        labels = labels_cpu
-      end
-      local cost = criterion:forward(model:forward(data), labels)
-      model:zeroGradParameters()
-      model:backward(data, criterion:backward(model.output, labels))
-      model:updateParameters(eta)
-      print(string.format('Batch %d/%d: cost=%.2f', i, nBatches, cost))
+      local err = self:trainBatch(data_cpu, labels_cpu)
+      print(string.format('Batch %d/%d: cost=%.2f', i, nBatches, err))
     end
-    collectgarbage()
-    collectgarbage()
   end
+end
+
+function Trainer:trainBatch(data_cpu, labels_cpu)
+  if self.is_cuda then cutorch.synchronize() end
+  collectgarbage()
+  local model = self.model
+  local criterion = self.criterion
+  local data = self.data
+  local labels = self.labels
+  if self.is_cuda then
+    data:resize(data_cpu:size()):copy(data_cpu)
+    labels:resize(labels_cpu:size()):copy(labels_cpu)
+  else
+    data = data_cpu
+    labels = labels_cpu
+  end
+
+  local err, outputs
+  feval = function(x)
+    model:zeroGradParameters()
+    outputs = model:forward(data)
+    err = criterion:forward(outputs, labels)
+    local gradOutputs = criterion:backward(outputs, labels)
+    model:backward(data, gradOutputs)
+    return err, self.gradParameters
+  end
+  optim.sgd(feval, self.parameters, self.optimState)
+
+  if self.is_cuda then cutorch.synchronize() end
+  self.loss_epoch = self.loss_epoch + err
+  return err
 end
 
