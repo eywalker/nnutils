86b7d9441124cfd5d2e1159c0434e98aec30f219
Edgar Y. Walker <edgar.walker@gmail.com>
2016-06-05 02:51:22 -0500

    Add multithreaded training

--- trainer.lua
+++ trainer.lua
@@ -1,4 +1,5 @@
 require 'torch'
+require 'nn'
 require 'optim'
 local threads = require 'threads'
 
@@ -8,7 +9,7 @@ nnutils = nnutils or {}
 
 local Trainer = torch.class('nnutils.Trainer')
 
-function Trainer:__init(model, criterion, dataLoader)
+function Trainer:__init(model, criterion, dataLoader, nMinions)
   self.model = model
   self.parameters = nil
   self.gradParameters = nil
@@ -21,9 +22,25 @@ function Trainer:__init(model, criterion, dataLoader)
     dampening = 0.0,
     weightDecay = 0.0
   }
+  self.nMinions = nMinions or 10
 
   -- initialize to float
   self:float()
+
+  self:setupPool()
+end
+
+function Trainer:setupPool()
+  self.pool = threads.Threads(
+    self.nMinions,
+    function()
+      require 'nnutils'
+    end,
+    function(threadid)
+      dataLoader = self.dataLoader
+      print(string.format('Starting minion with id; %d', threadid))
+    end
+  )
 end
 
 function Trainer:getParameters()
@@ -33,6 +50,8 @@ function Trainer:getParameters()
 end
 
 function Trainer:cuda()
+  require 'cutorch'
+  require 'cunn'
   self.model:cuda()
   self.criterion:cuda()
   self:getParameters()
@@ -47,9 +66,10 @@ function Trainer:float()
 end
 
 
-function Trainer:train(batchSize, eta, nEpochs)
+function Trainer:train(batchSize, nEpochs)
+  self.dataTimer = torch.Timer() -- measure time to load data
+  self.timer = torch.Timer() -- measure time to complete minibatch
   nEpochs = nEpochs or 1
-  eta = eta or 0.01
   local data, labels
   if self.is_cuda then
     self.data = torch.CudaTensor()
@@ -58,28 +78,49 @@ function Trainer:train(batchSize, eta, nEpochs)
   local model = self.model
   local criterion = self.criterion
   for n=1,nEpochs do
+    self.epoch = n
     self.loss_epoch = 0.0
+    self.batchNumber = 0
     self.dataLoader:startEpoch(batchSize)
-    local nBatches = self.dataLoader:nBatches()
-    for i=1,nBatches do
-      local data_cpu, labels_cpu = self.dataLoader:getMiniBatch(i)
-      local err = self:trainBatch(data_cpu, labels_cpu)
-      print(string.format('Batch %d/%d: cost=%.2f', i, nBatches, err))
+    self.nBatches = self.dataLoader:nBatches()
+
+    self.dataTimer:reset()
+    for i=1,self.nBatches do
+      self.pool:addjob(
+        function()
+          return dataLoader:getMiniBatch(i)
+        end,
+        function(...)
+          self:trainBatch(...)
+        end
+      )
     end
+    self.pool:synchronize()
+    if self.is_cuda then cutorch.synchronize() end
+    self:saveModel()
   end
+end
+
+function Trainer:saveModel()
+  -- stub method: save model to disk
 end
 
 function Trainer:trainBatch(data_cpu, labels_cpu)
   if self.is_cuda then cutorch.synchronize() end
   collectgarbage()
+  local dataLoadingTime = self.dataTimer:time().real
+  self.timer:reset()
+
   local model = self.model
   local criterion = self.criterion
   local data = self.data
   local labels = self.labels
   if self.is_cuda then
+    -- transfer to GPU
     data:resize(data_cpu:size()):copy(data_cpu)
     labels:resize(labels_cpu:size()):copy(labels_cpu)
   else
+    -- reference passing on CPU
     data = data_cpu
     labels = labels_cpu
   end
@@ -93,10 +134,15 @@ function Trainer:trainBatch(data_cpu, labels_cpu)
     model:backward(data, gradOutputs)
     return err, self.gradParameters
   end
+  -- perform single step SGD
   optim.sgd(feval, self.parameters, self.optimState)
 
   if self.is_cuda then cutorch.synchronize() end
   self.loss_epoch = self.loss_epoch + err
-  return err
+  self.batchNumber = self.batchNumber + 1
+  print(string.format(
+        'Epoch: [%d][%d/%d]\tTime %.3f Err %.4f DataLoadingTime %.3f',
+         self.epoch, self.batchNumber, self.nBatches, self.timer:time().real, err, dataLoadingTime))
+  self.dataTimer:reset()
 end
 
