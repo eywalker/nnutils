9605bdf5f8f68cfad4f50a71bf84f3dd3d4de46a
Edgar Y. Walker <edgar.walker@gmail.com>
2016-06-08 15:48:57 -0500

    Add SwitchableContainer and StudentTeacher modules

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ nnutils = nnutils or {}
 require "nnutils.DataLoader"
 require "nnutils.ClassDataLoader"
 require "nnutils.MNISTLoader"
+require "nnutils.SwitchableContainer"
+require "nnutils.StudentTeacher"
 require "nnutils.minibatch"
 require "nnutils.processingchain"
 require "nnutils.processor"
