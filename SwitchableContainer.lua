c58baa082cb88d52edf118bc16943dc924987df7
Edgar Y. Walker <edgar.walker@gmail.com>
2016-06-08 15:46:44 -0500

    Add container that supports turning training ON/OFF

---
+++ SwitchableContainer.lua
@@ -0,0 +1,13 @@
+local SwitchableContainer, parent = torch.class('nn.SwitchableContainer', 'nn.Sequential')
+
+function SwitchableContainer:__init(trainable)
+  parent.__inist(self)
+  if trainable==nil then trainable=true end
+  self.trainable = trainable
+end
+
+function SwitchableContainer:accGradParameters(...)
+  if self.trainable then
+    parent.accGradParameters(self, ...)
+  end
+end
