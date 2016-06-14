caa6f0ebc03abe2e738b62857a75d619ecb72161
Edgar Y. Walker <edgar.walker@gmail.com>
2016-06-14 16:31:26 -0500

    Update FFCriterion to yield mean input and return 1/n as gradient

--- FFCriterion.lua
+++ FFCriterion.lua
@@ -13,15 +13,15 @@ local FFCriterion, parent = torch.class('nn.FFCriterion', 'nn.Criterion')
 function FFCriterion:__init(minimize)
     parent.__init(self)
     self.sign = (minimize==nil or minimize) and 1 or -1
+    self.output = torch.Tensor()
     self.gradInput = torch.Tensor()
 end
 
 function FFCriterion:updateOutput(input, target)
-    self.output = input
-    return self.output
+    return input:mean()
 end
 
 function FFCriterion:updateGradInput(input, target)
-    self.gradInput:resize(input:size()):fill(1):mul(self.sign)
+    self.gradInput:resize(input:size()):fill(1):div(input:size(1)):mul(self.sign)
     return self.gradInput
 end
